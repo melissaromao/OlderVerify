7fb4df737affd7e4b02e4c08b7f49847e03bc079
Melissa Romão <86786053+melissaromao@users.noreply.github.com>
2024-09-03 14:49:12 -0300

    Add files via upload

---
+++ src/com/olderverifiy/age/AgeClassification.java
@@ -0,0 +1,35 @@
+package com.olderverifiy.age;
+
+//importando classe fundamental para leitura de entradas de dados
+import java.util.Scanner;
+
+public class AgeClassification {
+
+	public static void main(String[] args) {
+		// criando objeto Scanner para ler dados do teclado
+		Scanner sc = new Scanner(System.in);
+		// declarando variáveis
+		int idade;
+		String nome;
+		// exibindo mensagem
+		System.out.print("... Older Verify ...\n");
+		System.out.print("Digite seu nome: ");
+		// lê o dado armazenado na variável nome (tipo string)
+		nome = sc.next();
+		System.out.print("Digite sua idade: ");
+		// lê o dado armazenado na variável idade (tipo inteiro)
+		idade = sc.nextInt();
+		// estrutura de decisão (condicional) para verificação de idade
+		if (idade >= 60) {
+			System.out.print(nome + ", você é classificado(a) como idoso(a).");
+		} else if (idade >= 18) {
+			System.out.print(nome + ", você é classificado(a) como adulto(a).");
+		} else if (idade >= 13) {
+			System.out.print(nome + ", você é classificado(a) como adolescente.");
+		} else {
+			System.out.print(nome + ", você é classificado(a) como criança.");
+		}
+		// encerrando objeto para liberar recursos
+		sc.close();
+	}
+}
